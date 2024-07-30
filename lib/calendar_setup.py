c1d0359adc77ece4298883f9c237e14a618ee7f0
Colin Agner <80525280+notderpo@users.noreply.github.com>
2024-07-30 22:31:29 +0200

    Setup Calendar API Connection

--- lib/calendar_setup.py
+++ lib/calendar_setup.py
@@ -1,1 +1,22 @@
-# Do this
+
+import os
+
+from dotenv import load_dotenv
+from nylas import Client
+
+load_dotenv()
+
+CALENDAR_KEY = os.getenv('NYLAS_API_KEY')
+CALENDAR_URI = os.getenv('NYLAS_API_URI')
+
+
+
+nylas = Client(
+    api_key=CALENDAR_KEY,
+    api_uri=CALENDAR_URI
+)
+
+
+application = nylas.applications.info()
+application_id = application[1]
+print("Application ID:", application_id) 
